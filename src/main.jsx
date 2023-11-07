6fc151b872711a19659dbd49f1d44273d3eced12
Rakib Hossen <rakibhossen1660@gmail.com>
2023-11-07 10:43:37 +0600

    create register page

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import HomePage from './pages/home/HomePage.jsx'
+import RegisterPage from './pages/register/RegisterPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HomePage />
+    <RegisterPage />
   </React.StrictMode>,
 )
