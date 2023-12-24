43aed3d732d918fad037fe021bc166e399c06d7e
rakibhossen11 <rakibhossen1660@gmail.com>
2023-12-24 16:38:41 +0600

    create some update

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -12,8 +12,7 @@ const Hero = () => {
           Read the most interesting articles
         </h1>
         <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua
+          We provide not only article or story. We want to provide a emotion.
         </p>
         {/* hero search and title section */}
         <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
